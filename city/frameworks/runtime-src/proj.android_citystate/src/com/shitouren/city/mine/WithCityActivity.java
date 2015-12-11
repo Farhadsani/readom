30bcfd92b1ccc6d1841c272f915e3823ba1e0e50
lihuan123 <lihuanhuan@shitouren.com>
2015-12-11 12:19:23 +0800

    dev

--- city/frameworks/runtime-src/proj.android_citystate/src/com/shitouren/city/mine/WithCityActivity.java
+++ city/frameworks/runtime-src/proj.android_citystate/src/com/shitouren/city/mine/WithCityActivity.java
@@ -19,6 +19,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 import android.widget.PopupWindow;
+import android.widget.PopupWindow.OnDismissListener;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -26,9 +27,9 @@ public class WithCityActivity extends Activity implements OnClickListener {
 
 	LinearLayout llWithCityShare,llWithCityWeiXin,llWithCityWeiBo,llWithCityFeedback,llWithCityHaoPing;
 	int mHeigh;
-	private ImageView ivShareFriend;
-	private ImageView ivShareWeiBo;
-	private ImageView ivShareQQ;
+	private ImageView tvShareFriend;
+	private ImageView tvShareWeiBo;
+	private ImageView tvShareQQ;
 	private RelativeLayout rlShareCancel;
 	private PopupWindow window;
 	@Override
@@ -42,6 +43,13 @@ public class WithCityActivity extends Activity implements OnClickListener {
 		
 		initview();
 	}
+	
+//	@Override
+//	protected void onStart() {
+//		// TODO Auto-generated method stub
+//		backgroundAlpha(1.0f);
+//		super.onStart();
+//	}
 	private void initview() {
 		// TODO Auto-generated method stub
 		llWithCityShare = (LinearLayout) findViewById(R.id.llWithCityShare);
@@ -59,8 +67,20 @@ public class WithCityActivity extends Activity implements OnClickListener {
 		
 	}
 	
+	/** 
+     * 设置添加屏幕的背景透明度 
+     * @param bgAlpha 
+     */  
+    public void backgroundAlpha(float bgAlpha)  
+    {  
+        WindowManager.LayoutParams lp = getWindow().getAttributes();  
+        lp.alpha = bgAlpha; //0.0-1.0  
+        getWindow().setAttributes(lp);  
+    }
+	
 	private void showSharePW() {
 		// TODO Auto-generated method stub
+		backgroundAlpha(0.3f);
 		LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View view = inflater.inflate(R.layout.mine_with_city_share_popupwindow, null);
 		
@@ -80,21 +100,36 @@ public class WithCityActivity extends Activity implements OnClickListener {
 	    window.showAtLocation(WithCityActivity.this.findViewById(R.id.llWithCityShare),
 	        Gravity.BOTTOM, 0, 0);
 	    
-	    ivShareFriend = (ImageView) view.findViewById(R.id.ivShareFriend);
-	    ivShareWeiBo = (ImageView) view.findViewById(R.id.ivShareWeiBo);
-	    ivShareQQ = (ImageView) view.findViewById(R.id.ivShareQQ);
+	    window.setOnDismissListener(new OnDismissListener() {
+			@Override
+			public void onDismiss() {
+				// TODO Auto-generated method stub
+				rlShareCancel.setBackgroundResource(R.drawable.withcitysharequxiao);
+				backgroundAlpha(1.0f);
+				
+			}
+		});
+	    
+	    tvShareFriend =  (ImageView) view.findViewById(R.id.tvShareFriend);
+	    tvShareWeiBo = (ImageView) view.findViewById(R.id.tvShareWeiBo);
+	    tvShareQQ =  (ImageView) view.findViewById(R.id.tvShareQQ);
 	    rlShareCancel = (RelativeLayout) view.findViewById(R.id.rlShareCancel);
-	    ivShareFriend.setImageResource(R.drawable.withcitysharepengyouquan);
-	    ivShareFriend.setOnClickListener(this);
-		ivShareWeiBo.setOnClickListener(this);
-		ivShareQQ.setOnClickListener(this);
+	    
+	    tvShareFriend.setOnClickListener(this);
+	    tvShareWeiBo.setOnClickListener(this);
+	    tvShareQQ.setOnClickListener(this);
 		rlShareCancel.setOnClickListener(this);
+		tvShareFriend.setBackgroundResource(R.drawable.withcitysharepengyouquanweixuanzhong);
+		tvShareWeiBo.setBackgroundResource(R.drawable.withcityshareweiboweixuanzhong);
+		tvShareQQ.setBackgroundResource(R.drawable.withcityshareqqkongjianweixuanzhong);
+		rlShareCancel.setBackgroundResource(R.drawable.withcitysharequxiaoweixuanzhong);
 	}
 	
 	
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
+		
 		switch (v.getId()) {
 		case R.id.llWithCityShare:
 			showSharePW();
@@ -111,21 +146,37 @@ public class WithCityActivity extends Activity implements OnClickListener {
 		case R.id.llWithCityHaoPing:
 			
 			break;
-
+			
 			/**
 			 * PopupWindow选择键
 			 */
 			//分享朋友
-		case R.id.ivShareFriend:
+		case R.id.tvShareFriend:
+			tvShareFriend.setBackgroundResource(R.drawable.withcitysharepengyouquan);
+			tvShareWeiBo.setBackgroundResource(R.drawable.withcityshareweiboweixuanzhong);
+			tvShareQQ.setBackgroundResource(R.drawable.withcityshareqqkongjianweixuanzhong);
+			rlShareCancel.setBackgroundResource(R.drawable.withcitysharequxiaoweixuanzhong);
 			break;
 			//分享微博
-		case R.id.ivShareWeiBo:
+		case R.id.tvShareWeiBo:
+			tvShareWeiBo.setBackgroundResource(R.drawable.withcityshareweibo);
+			tvShareFriend.setBackgroundResource(R.drawable.withcitysharepengyouquanweixuanzhong);
+			tvShareQQ.setBackgroundResource(R.drawable.withcityshareqqkongjianweixuanzhong);
+			rlShareCancel.setBackgroundResource(R.drawable.withcitysharequxiaoweixuanzhong);
 			break;
 			//分享QQ
-		case R.id.ivShareQQ:
+		case R.id.tvShareQQ:
+			tvShareQQ.setBackgroundResource(R.drawable.withcityshareqqkongjian);
+			tvShareFriend.setBackgroundResource(R.drawable.withcitysharepengyouquanweixuanzhong);
+			tvShareWeiBo.setBackgroundResource(R.drawable.withcityshareweiboweixuanzhong);
+			rlShareCancel.setBackgroundResource(R.drawable.withcitysharequxiaoweixuanzhong);
 			break;
 			//分享取消
 		case R.id.rlShareCancel:
+			rlShareCancel.setBackgroundResource(R.drawable.withcitysharequxiao);
+			tvShareFriend.setBackgroundResource(R.drawable.withcitysharepengyouquanweixuanzhong);
+			tvShareWeiBo.setBackgroundResource(R.drawable.withcityshareweiboweixuanzhong);
+			tvShareQQ.setBackgroundResource(R.drawable.withcityshareqqkongjianweixuanzhong);
 			window.dismiss();
 			break;
 		default:
